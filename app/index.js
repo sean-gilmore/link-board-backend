32e8784bee46e2f4be2f4e770737735ac57b4834
Sean <gilmore.seven@gmail.com>
2019-05-05 19:16:51 +1000

    clean up Link methods, update routes

--- app/index.js
+++ app/index.js
@@ -1,16 +1,31 @@
 import Link from './models/link.js';
-const express = require('express')
+import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json())
+
 // Routes
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/v1/link/test', (req, res) => res.send(Link.test()));
-app.get('/v1/link/:id', async (req, res) => {
-  const result = await Link.get(req.params.id);
+
+// Links
+
+app.get('/v1/links', async (req, res) => {
+  const result = await Link.all();
+  res.json(result)
+})
+
+app.post('/v1/links/create', async (req, res) => {
+  console.log(req.body);
+  const result = await Link.create(req.body);
   res.json(result);
-});
-app.post('/v1/link/create',  (req, res) => res.send(Link.create(req.body)));
+})
+
+app.get('/v1/links/:id', async (req, res) => {
+  const result= await Link.get(req.params.id);
+  res.json(result);
+})
 
 // App start
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- app/models/link.js
+++ app/models/link.js
@@ -2,15 +2,14 @@ import knex from '../database';
 
 class Link {
   static create(newLink) {
-    knex('link').insert({"url": "http://google.com.au"});
-    return console.log(knex);
-  }
-
-  static test() {
-    return 'test of link class'
+    return knex('link').insert(newLink);
   }
 
   static get(linkId) {
+    return knex.from('link').select().where('id', linkId);
+  }
+
+  static all() {
     return knex.from('link').select().then(
       (rows) => {
         return rows;
