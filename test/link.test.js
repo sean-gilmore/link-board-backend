5a7c11bd25b0a1f6a82c1dbf933090e647fa921d
Sean <gilmore.seven@gmail.com>
2019-07-28 09:39:08 +1000

    configure mocha to run es6 js

--- test/link.test.js
+++ test/link.test.js
@@ -2,8 +2,8 @@ import Link from '../app/models/Link';
 
 describe('Link', function() {
   describe('get Link', function () {
-    it('should return a link', function() {
-      const link = Link.get(1);
+    it('should return a link', async function() {
+      const link = await Link.get(1)
       console.log(link);
     });
   })
